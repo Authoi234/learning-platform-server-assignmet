f3d08b531ad9802803b322ba96349beeba12a5bf
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-07 02:18:48 +0630

    courses is showed

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+const courses = require('./courses.json')
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('My server is running')
+})
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.listen(port, () => {
+  console.log(`My server is running on port ${port}`)
+})
