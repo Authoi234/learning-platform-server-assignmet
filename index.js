6d05cf919f8b11508d38fb037c9e6b76c1da1deb
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-09 00:30:59 +0630

    courses data is ready

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/courses', (req, res) => {
   res.send(courses)
 })
 
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourseDetail = 'a'
+})
+
 app.listen(port, () => {
   console.log(`My server is running on port ${port}`)
 })
